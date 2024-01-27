4bc1c8f664c7fdc078ff10037235219523627e53
Aditi191102 <105860777+Aditi191102@users.noreply.github.com>
2024-01-27 12:11:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    console.log('Form submitted:', formData);
+    
+    setFormData({
+      name:'',
+      email:'',
+      phone:'',
+      message:''
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contact Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter your name:
+          <br></br>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          Enter your email:
+          <br></br>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          Enter your phone number:
+          <br></br>
+          <input
+            type="tel"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          Enter your message:
+          <br></br>
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+        <button className='submitbtn' type="submit">Submit</button>
+      </form>
     </div>
   );
 }
